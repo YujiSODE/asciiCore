5fbb28dbb06191a60dc9993a01af6fdf69772edb
Yuji Sode <yuji.sode@gmail.com>
2018-12-05 09:17:00 +0900

    Add files via upload

--- test_asciiCore00.tcl
+++ test_asciiCore00.tcl
@@ -3,7 +3,7 @@ source -encoding utf-8 ./asciiCore.tcl;
 ::asciiCore::setEnvX wind [expr {-1.0*0.08}];
 ::asciiCore::setEnvY gravity [expr {9.8*0.08}];
 #################
-::asciiCore::setObject @ 1 {3 3} {1 0};
+::asciiCore::setIsolated [::asciiCore::setObject @ 1 {3 3} {1 0}];
 ::asciiCore::setObject * 1 {6 3} {-1 0};
 ::asciiCore::setObject D 1 {10 0} {0 1};
 ::asciiCore::setObject C 1 {10 4} {0 -0.5};
