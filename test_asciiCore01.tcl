051885a4b6d3ff65eb65ddffda01bb4e0bd91073
Yuji Sode <yuji.sode@gmail.com>
2018-12-08 15:36:40 +0900

    Add files via upload

--- test_asciiCore01.tcl
+++ test_asciiCore01.tcl
@@ -1,12 +1,10 @@
 #test_asciiCore01.tcl
 source -encoding utf-8 ./asciiCore.tcl;
 ::asciiCore::resize 20 10;
-#::asciiCore::setEnvX wind [expr {-1.0*0.08}];
 ::asciiCore::setEnvY gravity [expr {9.8*0.08}];
 #################
-::asciiCore::setObject * 1 {6 3};
-::asciiCore::setObject D 1 {4 0};
-::asciiCore::setObject C 1 {4 4} 
+::asciiCore::setObject A 1 {6 3};
+::asciiCore::setObject B 1 {4 0};
 #################
 set i 0;
 while {$i<10} {
@@ -16,4 +14,4 @@ while {$i<10} {
 #################
 ::asciiCore::run 10 1000;
 parray ::asciiCore::idMap;
-::asciiCore::output_JS;
+::asciiCore::output_JS {asciiCore_sample01};
